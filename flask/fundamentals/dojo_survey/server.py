32f639f6f80e791a914cf4a336ded965acd59365
Robert Johnson <rcj148@gmail.com>
2022-01-25 17:22:32 -0500

    updates

---
+++ flask/fundamentals/dojo_survey/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, session, request
+app = Flask(__name__)
+app.secret_key = 'aijefiljaljailjeiljfal;jl'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    print(request.form)
+    print(request.form['name'])
+    print(request.form['dojo'])
+    print(request.form['language'])    
+
+    # Never render a template on a POST request.
+    # Instead we will redirect to our index route
+    session["name"] = request.form['name']
+    session["dojo"] = request.form['dojo']
+    session["language"] = request.form['language']
+    session["comment"] = request.form['comment']
+    return redirect('/result')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
