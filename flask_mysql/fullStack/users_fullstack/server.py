ae3863a2c04ee15c76d00d438c3fa62b19352dc4
Robert Johnson <rcj148@gmail.com>
2022-01-28 17:18:41 -0500

    updated

--- flask_mysql/fullStack/users_fullstack/server.py
+++ flask_mysql/fullStack/users_fullstack/server.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 app.secret_key = 'alkfjjailfjajliejliajlij'
 from user import User
 
+#============================================
+# 1. Get all users from database
+#============================================
+
 @app.route('/')
 def index():
 
@@ -15,11 +19,15 @@ def index():
 
     return render_template('index.html', users =  users)
 
-@app.route("/add_user")
+#============================================
+# 2. Get add one user from database
+#============================================
+
+@app.route("/user/add_user")
 def add_user():
     return render_template("add_user.html")
 
-@app.route("/create_user", methods=["POST"])
+@app.route("/user/create_user", methods=["POST"])
 def create_user():
 
     #1 collect info from Form
@@ -35,10 +43,57 @@ def create_user():
     }
     
     #3 call on query
-    add_user_id = User.create_user(data)
-    
+    User.create_user(data)
+        
     #4 if successful, redirect to render route
     return redirect("/")
+
+# #============================================
+# # 3. Get one user from database
+# #============================================
+
+@app.route("/user/show/<int:user_id>")
+def user_info(user_id):
+    # Call on one_user query
+    data = {
+        "user_id" : user_id
+    }
+    user = User.one_user(data)
+
+    return render_template("user_info.html", user = user)
+
+# #============================================
+# # 4. Edit one user from database
+# #============================================
+@app.route("/user/edit/<int:user_id>")
+def edit_user(user_id):
+    #1 collect info from Form
+    #2 repack info to send to query
+    data = {
+        "user_id" : user_id
+    }
+    print(User.one_user(data))
+    #3 call on query
+    return render_template("edit_user.html", user = User.one_user(data))
+
+@app.route("/user/update", methods=['POST'])
+def update():
+    
+    User.update(request.form)
+    # print(user)
+    return redirect("/")
+
+# #============================================
+# # 5. Delete one user from database
+# #============================================
+
+@app.route("/user/delete/<int:user_id>")
+def delete(user_id):
+    data = {
+        "user_id" : user_id
+    }
+
+    return render_template("user_info.html", user = User.delete(data))
 
 if __name__=="__main__":
     app.run(debug=True)
--- flask_mysql/fullStack/users_fullstack/user.py
+++ flask_mysql/fullStack/users_fullstack/user.py
@@ -13,13 +13,12 @@ class User:
         self.updated_at = data['updated_at']
     
     #============================================
-    #Get all Users from database
+    # 1. Get all Users from database
     #============================================
     # Now we use class methods to query our database
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('users_schema').query_db(query)
         print(results)
         # Create an empty list to append our instances of friends
@@ -30,7 +29,7 @@ class User:
         return users
 
     #============================================
-    #Get all Users from database
+    # 2. Creat one user in database
     #============================================
 
     @classmethod
@@ -42,3 +41,38 @@ class User:
         results = connectToMySQL('users_schema').query_db(query, data)
         print(results)
         return results
+
+    # #============================================
+    # # 3. Get one user from database
+    # #============================================
+
+    @classmethod
+    def one_user(cls, data):
+        query = "SELECT * FROM users WHERE id = %(user_id)s;"
+        results = connectToMySQL('users_schema').query_db(query, data)
+        # print(results)
+        return cls(results[0])
+
+    # #============================================
+    # # 4. Edit one user from database
+    # #============================================
+
+    @classmethod
+    def update(cls, data):
+        query = """
+        UPDATE users 
+        SET 
+            first_name = %(first_name)s, 
+            last_name = %(last_name)s, 
+            email = %(email)s,
+            updated_at = NOW()
+        WHERE id = %(edit_user)s;
+        """
+        # results = connectToMySQL('users_schema').query_db(query, data)
+        # print(results)
+        return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id = %(user_id)s;"
+        return connectToMySQL('users_schema').query_db(query, data)
